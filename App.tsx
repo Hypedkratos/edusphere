11393218be126a6391efeb113c87a1b360088065
Hypedkratos <shashianand8651@gmail.com>
2023-12-03 09:14:12 +0530

    made changes to loginscreen, added  new homescreen

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import Loginscreen from './screens/Loginscreen'
+import Homescreen from './screens/Homescreen'
 
 const Stack = createStackNavigator();
 
@@ -11,6 +12,7 @@ export default function App (){
     <NavigationContainer>
       <Stack.Navigator>
       <Stack.Screen options={{headerShown:false}} name='Loginscreen' component={Loginscreen}></Stack.Screen>
+      <Stack.Screen options={{headerShown:false}} name='Homescreen' component={Homescreen}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   )
--- screens/loginscreen.tsx
+++ screens/loginscreen.tsx
@@ -1,14 +1,79 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity, Linking } from 'react-native'
+import React, {useState} from 'react'
+import { useNavigation } from '@react-navigation/native'
+import Homescreen from './Homescreen'
 
 const Loginscreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  const handleLogin = () =>{  
+  console.log('Email:', email);
+  console.log('Password:', password);
+  //Logic for submission goes here
+  
+  }
+  const navigateToPickupscreen = () => {
+    navigation.navigate('Homescreen');
+  };
+
+  const navigation=useNavigation();
+  
   return (
-    <View>
-      <Text style={{color:'#000', fontSize:24}}>E humara Loginscreen hai!</Text>
-    </View>
+    <KeyboardAvoidingView style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        keyboardType="email-address"
+        autoCapitalize="none"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+      />
+       <TouchableOpacity style={styles.button} 
+       onPress={()=>{
+        handleLogin();
+        navigateToPickupscreen();
+      }}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
   )
 }
 
 export default Loginscreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  }, 
+  input:{
+    height: 40,
+    borderColor: 'blue',
+    borderWidth: 1,
+    marginBottom: 16,
+    padding: 8,
+    width: '100%',
+  },
+  button:{
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 20,
+    width: '100%',
+    alignItems: 'center',
+  },
+  buttonText:{
+    color: 'white',
+    fontSize: 16,
+  }
+})
